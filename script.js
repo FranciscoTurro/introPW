f34bd8fc1acae7765a47b2b31a97feba66133230
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-11-13 16:49:56 -0300

    a

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ if (noticiaSsd) {
   });
 }
 
+const noticiaEth = document.querySelector('.noticiaEth');
+if (noticiaEth) {
+  noticiaEth.addEventListener('click', () => {
+    document.location.href = './paginas/noticiaEth.html';
+  });
+}
+
+const noticiaSec = document.querySelector('.noticiaSecurity');
+if (noticiaSec) {
+  noticiaSec.addEventListener('click', () => {
+    document.location.href = './paginas/noticiaSecurity.html';
+  });
+}
+
 const imgChat = document.querySelector('.imgChat');
 if (imgChat) {
   imgChat.addEventListener('click', () => {
