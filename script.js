a6c14b699a2c520ab2c622a772e25fbacbb9ee29
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-11-13 13:52:13 -0300

    a

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const noticiaSsd = document.querySelector('.noticiaSsd');
+if (noticiaSsd) {
+  noticiaSsd.addEventListener('click', () => {
+    document.location.href = './paginas/noticiaSsd.html';
+  });
+}
+
 const imgChat = document.querySelector('.imgChat');
-imgChat.addEventListener('click', () => {
-  alert('Chat en vivo');
-});
+if (imgChat) {
+  imgChat.addEventListener('click', () => {
+    alert('Chat en vivo');
+  });
+}
