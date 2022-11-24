d3791a28089eb787200f7f44d4b97b37bf3701e3
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-11-24 18:15:53 -0300

    aa

--- tp JS Turró/index.js
+++ tp JS Turró/index.js
@@ -5,16 +5,41 @@ const btnAlta = document.querySelector('.btnAlta');
 const btnBaja = document.querySelector('.btnBaja');
 const btnModi = document.querySelector('.btnModi');
 const tablaEmpleados = document.querySelector('#tablaEmpleados');
-const legajoConsu = document.querySelector('#legajoConsu');
-const btnLegajoConsu = document.querySelector('#btnLegajoConsu');
-const resultadoConsulta = document.querySelector('#resultadoConsulta');
+const legajoConsu1 = document.querySelector('#legajoConsu1');
+const btnLegajoConsu1 = document.querySelector('#btnLegajoConsu1');
+const resultadoConsulta1 = document.querySelector('#resultadoConsulta1');
 const legajoVent = document.querySelector('#legajoVent');
 const fecha = document.querySelector('#fecha');
 const importe = document.querySelector('#importe');
 const btnAltaVentas = document.querySelector('.btnAltaVentas');
+const legajoConsu2 = document.querySelector('#legajoConsu2');
+const btnLegajoConsu2 = document.querySelector('#btnLegajoConsu2');
+const resultadoConsulta2 = document.querySelector('#resultadoConsulta2');
 
-let arrayEmpleados = [];
-let arrayVentas = [];
+let arrayEmpleados = [
+  {
+    nombre: 'a',
+    apellido: 'a',
+    legajo: 'a',
+  },
+  {
+    nombre: 'b',
+    apellido: 'b',
+    legajo: 'b',
+  },
+];
+let arrayVentas = [
+  {
+    fecha: '22-11-22',
+    importe: '2',
+    legajo: 'a',
+  },
+  {
+    fecha: '21-11-22',
+    importe: '14',
+    legajo: 'a',
+  },
+];
 
 //googlear como hacer una html table con la info de un array.
 //las ventas son un array aparte
@@ -110,17 +135,59 @@ btnAltaVentas.addEventListener('click', () => {
       alert('ERROR: No existe un empleado con el legajo ingresado');
     }
   } else {
-    alert('ERROR: Complete todos los campos para dar de alta una venta');
+    alert(
+      'ERROR: Complete todos los campos de forma correcta para dar de alta una venta'
+    );
   }
 });
 
-btnLegajoConsu.addEventListener('click', () => {
+btnLegajoConsu1.addEventListener('click', () => {
   const legajoRepetido = arrayEmpleados.find(
-    (empleado) => empleado.legajo === legajoConsu.value
+    (empleado) => empleado.legajo === legajoConsu1.value
   );
-  if (legajoConsu.value !== '' && legajoRepetido !== undefined) {
-    legajoConsu.value = '';
-    resultadoConsulta.innerHTML = `Nombres del empleado con legajo ingresado: ${legajoRepetido.apellido}, ${legajoRepetido.nombre}`;
+  if (legajoConsu1.value !== '' && legajoRepetido !== undefined) {
+    legajoConsu1.value = '';
+    resultadoConsulta1.innerHTML = `Nombres del empleado con legajo ingresado: ${legajoRepetido.apellido}, ${legajoRepetido.nombre}`;
+  } else {
+    alert(
+      'ERROR: Ingrese un numero de legajo valido para realizar una consulta'
+    );
+  }
+});
+
+btnLegajoConsu2.addEventListener('click', () => {
+  const legajoRepetido = arrayEmpleados.find(
+    (empleado) => empleado.legajo === legajoConsu2.value
+  );
+  if (legajoConsu2.value !== '' && legajoRepetido !== undefined) {
+    const ventasEmpleado = arrayVentas.filter(
+      (venta) => venta.legajo === legajoConsu2.value
+    );
+
+    let txt = '';
+    let total = 0;
+    let i = 1;
+    ventasEmpleado.forEach((venta) => {
+      txt += `Venta ${i} con importe $${venta.importe} en el ${venta.fecha}. `;
+      i++;
+      total += parseInt(venta.importe);
+    });
+
+    legajoConsu2.value = '';
+    resultadoConsulta2.innerHTML = txt + `Importe total: $${total}`;
+  } else {
+    alert(
+      'ERROR: Ingrese un numero de legajo valido para realizar una consulta'
+    );
+  }
+});
+
+btnLegajoConsu3.addEventListener('click', () => {
+  //quiere ver cada vendedor, el subtotal (suma de sus ventas) y el total de todos los vendedores
+  const legajoRepetido = arrayEmpleados.find(
+    (empleado) => empleado.legajo === legajoConsu3.value
+  );
+  if (legajoConsu3.value !== '' && legajoRepetido !== undefined) {
   } else {
     alert(
       'ERROR: Ingrese un numero de legajo valido para realizar una consulta'
